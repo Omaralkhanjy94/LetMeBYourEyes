a058368558b848b1c6376dbb878f792ef300b2c3
Omaralkhanjy94 <45111028+Omaralkhanjy94@users.noreply.github.com>
2022-10-11 00:52:44 +0300

    Add files via upload

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MainColors {
-  static const Color primaryColor = Colors.indigo;
-  static const Color secondColor = Colors.cyan;
+  static const Color primaryColor = Colors.cyan;
+  static const Color secondColor = Colors.indigo;
 
   // static const MaterialStateProperty<Color?>? mlightBlueGreen = Color.fromRGBO(176, 223, 229, 1);
 }
